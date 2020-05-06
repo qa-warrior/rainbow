9cb33856dba74694b9605b9dec330ba2dffa3125
Michael Demarais <m.demarais@gmail.com>
2020-05-06 05:32:55 -0400

    begin refactor charts

--- src/screens/Routes/Routes.ios.js
+++ src/screens/Routes/Routes.ios.js
@@ -69,7 +69,7 @@ const importSeedPhraseFlowRoutes = {
   },
   [Routes.OVERLAY_EXPANDED_ASSET_SCREEN]: {
     navigationOptions: overlayExpandedPreset,
-    screen: ExpandedAssetScreenWithData,
+    screen: ModalScreen,
   },
 };
 
