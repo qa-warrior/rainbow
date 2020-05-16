76ae2b59a292fc6417aa36cdf7b1ca815b53f136
Michael Demarais <m.demarais@gmail.com>
2020-05-16 07:17:56 -0400

    SC-ify: previous Optimize core text components
    this commit refactors previous commit titled "Optimize core text components" to use styled-components

--- src/styles/buildTextStyles.js
+++ src/styles/buildTextStyles.js
@@ -1,9 +1,8 @@
 import { get, isNil } from 'lodash';
-import { useMemo } from 'react';
 import colors from './colors';
 import fonts from './fonts';
 
-export const buildTextStyles = ({
+export default function buildTextStyles({
   align,
   color,
   family = 'SFProRounded',
@@ -15,80 +14,48 @@ export const buildTextStyles = ({
   size,
   uppercase,
   weight = 'regular',
-}) => ({
-  // font family
-  ...(isEmoji ? {} : { fontFamily: fonts.family[mono ? 'SFMono' : family] }),
-  // font weight
-  ...(isEmoji || isNil(weight)
-    ? {}
-    : { fontWeight: get(fonts, `weight[${weight}]`, weight) }),
-  // letter spacing
-  ...(isNil(letterSpacing)
-    ? {}
-    : {
-        letterSpacing: get(
-          fonts,
-          `letterSpacing[${letterSpacing}]`,
-          letterSpacing
-        ),
-      }),
-  // line height
-  ...(isNil(lineHeight)
-    ? {}
-    : { lineHeight: get(fonts, `lineHeight[${lineHeight}]`, lineHeight) }),
-  // text align
-  ...(align ? { textAlign: align } : {}),
-  // uppercase
-  ...(uppercase ? { textTransform: 'uppercase' } : {}),
-  // color
-  color: colors.get(color) || colors.dark,
-  // font size
-  fontSize:
-    typeof size === 'number' ? size : parseFloat(fonts.size[size || 'medium']),
-  // opacity
-  opacity,
-});
-
-export default function useTextStyles({
-  align,
-  color,
-  family,
-  isEmoji,
-  letterSpacing,
-  lineHeight,
-  mono,
-  opacity,
-  size,
-  uppercase,
-  weight,
 }) {
-  return useMemo(
-    () =>
-      buildTextStyles({
-        align,
-        color,
-        family,
-        isEmoji,
-        letterSpacing,
-        lineHeight,
-        mono,
-        opacity,
-        size,
-        uppercase,
-        weight,
-      }),
-    [
-      align,
-      color,
-      family,
-      isEmoji,
-      letterSpacing,
-      lineHeight,
-      mono,
-      opacity,
-      size,
-      uppercase,
-      weight,
-    ]
-  );
+  return {
+    // font family
+    ...(isEmoji ? {} : { fontFamily: fonts.family[mono ? 'SFMono' : family] }),
+
+    // font weight
+    ...(isEmoji || isNil(weight)
+      ? {}
+      : { fontWeight: get(fonts, `weight[${weight}]`, weight) }),
+
+    // letter spacing
+    ...(isNil(letterSpacing)
+      ? {}
+      : {
+          letterSpacing: get(
+            fonts,
+            `letterSpacing[${letterSpacing}]`,
+            letterSpacing
+          ),
+        }),
+
+    // line height
+    ...(isNil(lineHeight)
+      ? {}
+      : { lineHeight: get(fonts, `lineHeight[${lineHeight}]`, lineHeight) }),
+
+    // text align
+    ...(align ? { textAlign: align } : {}),
+
+    // uppercase
+    ...(uppercase ? { textTransform: 'uppercase' } : {}),
+
+    // color
+    color: colors.get(color) || colors.dark,
+
+    // font size
+    fontSize:
+      typeof size === 'number'
+        ? size
+        : parseFloat(fonts.size[size || 'medium']),
+
+    // opacity
+    opacity,
+  };
 }
